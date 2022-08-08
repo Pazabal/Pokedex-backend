01cc3bea5c189137612ad6593e392fc3ab2e7ce9
santidiazfe <santidiazfe@gmail.com>
2022-08-08 20:43:25 -0300

    Final migrations and seeds

--- migrations/20220806184245_pokemonsxtypes.js
+++ migrations/20220806184245_pokemonsxtypes.js
@@ -6,9 +6,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('pokemonsxtypes', (table) => {
     table.increments('pokemonsxtypes_id');
     table.integer('pokemon_id').references('pokemon.id'); //tabla pokemon en columna id
-    table.integer('type1_id').references('types.id'); //1- nombre columna 2-a que tabla se hace referencia
-    table.integer('type2_id').references('types.id'); //se puede hacer opcional??
-    table.integer('type3_id').references('types.id');
+    table.integer('type_id').references('types.id'); //1- nombre columna 2-a que tabla se hace referencia
+   
   })
 };
 
