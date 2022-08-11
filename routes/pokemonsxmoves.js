51c4b84214fca7776b7584e79638172b84afd093
santidiazfe <santidiazfe@gmail.com>
2022-08-11 20:18:07 -0300

    modified controllers & routes

--- routes/pokemonsxmoves.js
+++ routes/pokemonsxmoves.js
@@ -5,18 +5,21 @@ const pokemonsxMovesQueries = require('../controllers/pokemonsxmoves')
 router.get('/', async (req, res) => {
     const pokemonsxMoves = await pokemonsxMovesQueries.getAllPokemonsxMoves();
     res.json(pokemonsxMoves)
+    
 })
 
 router.get('/:id' , async (req, res) => {
     const id = req.params.id
     const pokemonsxMoves = await pokemonsxMovesQueries.getPokemonsxMovesById(id);
     res.json(pokemonsxMoves)
+    
 })
 
 router.post('/' , async (req, res) => {
     const body = req.body
     const newPokemonsxMoves = await pokemonsxMovesQueries.createPokemonsxMoves(body);
     res.json(newPokemonsxMoves)
+    
 })
 
 router.put('/:id' , async (req, res) => {
@@ -24,12 +27,14 @@ router.put('/:id' , async (req, res) => {
     const body = req.body
     const updatePokemonsxMoves = await pokemonsxMovesQueries.updatePokemonsxMoves(id, body);
     res.json(updatePokemonsxMoves)
+    
 })
 
 router.delete('/:id' ,async (req, res) => {
     const id = req.params.id
-    const deletePokemonsxMoves = await pokemonsxMoves.deletePokemonsxMoves(id);
+    const deletePokemonsxMoves = await pokemonsxMovesQueries.deletePokemonsxMoves(id);
     res.json(deletePokemonsxMoves)
+    
 })
 
 module.exports = router;
