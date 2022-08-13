ea66913fee2b4c705914427905331b525e34c05c
Pazabal <81312828+Pazabal@users.noreply.github.com>
2022-08-12 22:07:59 -0300

    tests with put

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -8,12 +8,12 @@ const getAllPokemons = () => {
     .from('pokemon')
 }
 
-knex.select('*')
-    .from('pokemon')
-    .join('pokemonsxmoves', 'pokemon_id', 'pokemon.id')
-    .join('moves','id', 'pokemonsxmoves.moves_id')
-    .join('pokemonsxtypes', 'pokemon_id','pokemon.id')
-    .join('types', 'id', 'pokemonxtypes.type_id');
+// knex.select('*')
+//     .from('pokemon')
+//     .join('pokemonsxmoves', 'pokemon_id', 'pokemon.id')
+//     .join('moves','id', 'pokemonsxmoves.moves_id')
+//     .join('pokemonsxtypes', 'pokemon_id','pokemon.id')
+//     .join('types', 'id', 'pokemonsxtypes.type_id')
     
 
 
@@ -24,27 +24,86 @@ const getPokemonById = (id) => {
     .select('name', 'id', 'weight','height' ,'description', 'image', 'hp', 'atk', 'def', 'satk','sdef', 'spd')
 }
 
-const createPokemon = (body) => {
-    knex('pokemon')
-    .insert(body)
-    .returning('id')
-    // .where('id', '=', id)
-    .then( (id) => {
-        const pokemonsToInsertMoves = body.moves.map(move =>
-            ({moves_id: move.moves_id, pokemon_id: id}));
+// const createPokemon = (body) => {
+//     return knex('pokemon')
+//     .insert(body)
     
-         return knex('pokemonsxmoves').insert(pokemonsToInsertMoves)
-            // console.log("create pokemon");
+    // .returning('id')
+    // .where('id', '=', id)
+    // .then( (id) => {
+    //     const pokemonsToInsertMoves = body.moves.map(move =>
+    //         ({moves_id: move.moves_id, pokemon_id: id}));
+    
+    //      return knex('pokemonsxmoves').insert(pokemonsToInsertMoves)
+    //         // console.log("create pokemon");
 
-    })
-    .then( (id) =>{
-        const pokemonsToInsertTypes = body.types.map(type =>
-            ({types_id: type.id, pokemon_id: id}))
-        return knex('pokemonsxtypes').insert(pokemonsToInsertTypes)
-    })
+    // })
+    // .then( (id) =>{
+    //     const pokemonsToInsertTypes = body.types.map(type =>
+    //         ({types_id: type.id, pokemon_id: id}))
+    //     return knex('pokemonsxtypes').insert(pokemonsToInsertTypes)
+    // })
+// }
+    // const createPokemon = async (body) => {
+    //     await knex("pokemon")
+    //       .insert(body.pokemon)
+    //       .returning("id")
+    //       .then((ArrayConId) => {
+    //         const pokemonConHabilidades = body.moves.map((habilidad) => ({
+    //           pokemon_id: ArrayConId[0].id,
+    //           move_id: habilidad.id,
+    //         }));
+      
+    //         return pokemonConHabilidades;
+    //       })
+    //       .then((objetoPokemonMoves) => {
+    //         knex("pokemon_moves")
+    //           .insert(objetoPokemonMoves)
+    //           .then((respuesta) => {
+    //             res
+    //               .status(200)
+    //               .json({ mensaje: "el pokemon fue creado", response: respuesta });
+    //           });
+    //       });
+    //   };
+    const createPokemon = async (body) => {
+        return knex ('pokemon')
+        .insert(body.pokemon)
+        .returning('id')
+        .then((arrayDePokemon) => { //array de pokemones
+            const pokemonConHabilidades = body.moves.map((habilidad) => ({
+                pokemon_id: arrayDePokemon[0].id,
+                moves_id: habilidad.moves_id,
+            }));
+            return pokemonConHabilidades;
+        })
+        .then ((pokemonConHabilidades) => { //insert moves
+            knex('pokemonsxmoves')
+            .insert(pokemonConHabilidades)
+            .then((res) => {
+                console.log(res)
+            })
+        })
+        .then((arrayDePokemon) => {
+            const pokemonConHabilidades = body.types.map((habilidad) => ({
+                pokemon_id: arrayDePokemon[0].id,
+                type_id: habilidad.type_id,
+            }));
+            return pokemonConHabilidades;
+        })
+        .then ((pokemonConHabilidades) => {
+            knex('pokemonsxtypes')
+            .insert(pokemonConHabilidades)
+            .then((res) => {
+                console.log(res)
+            })
+        })
+    }
+    
+      
    
    
-}
+
 
 const updatePokemon = (id, body) => {
     return knex('pokemon')
---
+++ routes/auth.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {verifyToken, TOKEN_SECRET} = require('../middlewares/validate-jwt');
+const router = express.Router();
+
+const usuarios = [];
+
+//Registro usuarios
+router.post('/inicio', (req,res) => {
+    
+    //hash contraseña
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = {
+        name: req.body.name,
+        mail: req.body.mail,
+        password: password
+    }
+    usuarios.push(newUser);
+    res.json({success: true, newUser, usuarios});
+    //buscamos usuario con mismo mail
+    const user = usuarios.find((u) => u.mail === req.body.mail);
+    if(!user) {
+        return res.status(400).json({error: 'Usuario no encontrado'});
+    }
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) {
+        return res.status(400).json({error: 'Contraseña no válida'});
+    }
+    //Crear el token
+    const token = jwt.sign({
+        name: user.name,
+        id: user.id
+    }, TOKEN_SECRET);
+    res.json({error: null, data: 'Login exitoso', token});
+});
+
+//Peticion listar usuarios solo puede hacerla alguien autorizado
+router.get('/usuarios', verifyToken, async (req, res) => {
+    // datos del uuario
+    console.log(req.user);
+    res.json({error: null, usuarios});
+});
+
+module.exports = router;
